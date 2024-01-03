c8ad1db89d0d87f4da0311d7773471ebd23f64ae
Odarell35 <leratomawasha0@gmail.com>
2024-01-03 02:10:33 -0800

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
-
-"""script to export data in the JSON format."""
-
+"""export to CSV"""
+import csv
 import json
 import requests
 import sys
 
+def get_employee_todo_progress(employee_id):
+    """MEHTOD TO EXPORT"""
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{base_url}/users/{employee_id}'
+    todo_url = f'{base_url}/todos?userId={employee_id}'
+
+    # Fetch user information
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data.get('username')
+
+    # Fetch todo list
+    todo_response = requests.get(todo_url)
+    todo_data = todo_response.json()
+
+    # Prepare data for JSON export
+    user_tasks = []
+    for task in todo_data:
+        user_tasks.append({
+            'task': task['title'],
+            'completed': task['completed'],
+            'username': employee_name
+        })
+
+    # Export to JSON
+    filename = f"{employee_id}.json"
+    with open(filename, 'w') as jsonfile:
+        json.dump({employee_id: user_tasks}, jsonfile)
 
 if __name__ == "__main__":
-
-    session = requests.Session()
-
-    _id = sys.argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(_id)
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(_id)
-
-    emp_id = session.get(base_url)
-    emp_name = session.get(user_url)
-
-    json_results = emp_id.json()
-    usernam = emp_name.json()['username']
-
-    Tasks = []
-    new_dict = {}
-
-    for employees in json_results:
-        Tasks.append(
-            {
-                "task": employees.get('title'),
-                "completed": employees.get('completed'),
-                "username": new_dict,
-            })
-    new_dict[_id] = Tasks
-
-    filename = _id + ".json"
-    with open(filename, 'w') as f:
-        json.dump(new_dict, f)
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
