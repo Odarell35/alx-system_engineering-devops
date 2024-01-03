027cd8706f36b50c1f2aa87c753c86655edf37a6
Odarell35 <leratomawasha0@gmail.com>
2024-01-03 05:24:13 -0800

    pycodestyle

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -15,26 +15,22 @@ def get_employee_todo():
     user_response = requests.get(user_url)
     user_data = user_response.json()
     todo_all_employees = {}
-    
-    #fetch each users todo list
+
     for user in user_data:
         user_id = user['id']
         username = user['username']
         todo_url = f'{base_url}/todos?userId={user_id}'
-    
-    # Fetch todo list all users
-    todo_response = requests.get(todo_url)
-    todo_data = todo_response.json()
-
-    # Prepare data for JSON export
-    user_tasks = []
-    for task in todo_data:
-        user_tasks.append({
-            'username': username,
-            'task': task['title'],
-            'completed': task['completed'],
-         
-        })
+        # Fetch todo list all users
+        todo_response = requests.get(todo_url)
+        todo_data = todo_response.json()
+        # Prepare data for JSON export
+        user_tasks = []
+        for task in todo_data:
+            user_tasks.append({
+                'username': username,
+                'task': task['title'],
+                'completed': task['completed'],
+                 })
 
         todo_all_employees[user_id] = user_tasks
 
