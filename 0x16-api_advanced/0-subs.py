3a3fb8e1f976645203ddfcb5dca55da79ec14ab5
Odarell35 <leratomawash0@gmail.com>
2024-01-16 13:13:45 +0200

    tasks

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""0-subs Module"""
+
+import requests
+
+headers = {"User-Agent": "MyRedditBot/1.0"}
+
+
+def number_of_subscribers(subreddit):
+	"""number of subs"""
+	url = f'https://www.reddit.com/r/{subreddit}/about.json'
+	response = requests.get(url, allow_redirects=False, headers=headers)
+	if response.status_code == 200:
+		data = response.json()
+		return data['data']['subscribers']
+	else:
+		return 0 
