25d25d01658a7156fd3fa7347196c36d496bf8ec
Odarell35 <leratomawash0@gmail.com>
2024-01-16 14:43:05 +0200

    pep 8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ headers = {"User-Agent": "MyRedditBot/1.0"}
 
 
 def number_of_subscribers(subreddit):
-	"""number of subs"""
-	url = f'https://www.reddit.com/r/{subreddit}/about.json'
-	response = requests.get(url, allow_redirects=False, headers=headers)
-	if response.status_code == 200:
-		data = response.json()
-		return data['data']['subscribers']
-	else:
-		return 0 
+    """number of subs"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
